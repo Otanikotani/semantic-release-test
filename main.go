9ca937568b3a72703605290f5e262f5c38f5d0cb
Andrey Artyukhov <otanikotani@gmail.com>
2020-02-26 14:42:05 -0500

    fix: remove stuff

--- main.go
+++ main.go
@@ -6,10 +6,4 @@ import (
 
 func main() {
 	fmt.Println("Hello world")
-	fmt.Println("Hello world 2")
-	fmt.Println("Hello world 2")
-	fmt.Println("Hello world 2")
-	fmt.Println("Hello world 2")
-	fmt.Println("Hello world 2")
-
 }
